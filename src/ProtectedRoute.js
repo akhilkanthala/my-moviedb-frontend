597ae9c751e3e4a6b2ad08614bdd9ecd0ca6cc07
akhilkanthala <akhil.kanthala@zemosolabs.com>
2019-11-07 11:46:17 +0530

    remove node modules

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "./react-auth0-spa/react-auth0-spa";
 
 export const ProtectedRoute = ({component:Component, ...rest}) => {
   const { isAuthenticated} = useAuth0();
-  return (
+   return (
     <Route
       {...rest}
       render={props => {
